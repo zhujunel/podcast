e2fdca9bbcc9f4aa94dc3789377cc113f7cd106f
baisheng <baisheng@outlook.com>
2017-08-17 09:53:19 +0800

    First commit

---
+++ src/mocks/data.js
@@ -0,0 +1,197 @@
+module.exports = [{
+  id: '2',
+  anchor: {
+    name: '叶青',
+    detail: '高冷鲜师 理性思辨者 幕后英雄',
+    avatar: 'https://media.alearning.com.cn/podcast/book/2/anchor.jpg'
+  },
+  author: {
+    avatar: 'https://media.alearning.com.cn/podcast/book/2/author.jpg',
+    name: '海姆·G·吉诺特',
+    detail: '海姆·G·吉诺特，心理学博士、临床心理家、儿童心理学家、儿科医生；纽约大学研究生院兼职心理教授、艾德尔菲大学博士后指导教授。 吉诺特博士的一生并不长，他将其短短的一生致力于儿童心理的研究以及对父母和教师的教育。他依靠他的职明才智做了很多创造性的工作，取得了巨大的成就。他在他的书中、演讲中、专栏中宣传的如何跟孩子沟通的创新思想不仅在美国，甚至在全世界都获得巨大反响。'
+  },
+  cover: 'https://media.alearning.com.cn/podcast/book/2/cover.jpg',
+  name: '孩子，把你的手给我',
+  title: '孩子，把你的手给我',
+  type: 'book',
+  status: '',
+  list: [{
+    'songname': '作者介绍、朗读者介绍',
+    'enworkid': '/s/zMJoBsEtQWaw8zZigcsrjA',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/201.mp3',
+    'type': 'MP3'
+  }, {
+    'songname': '交流密码：我说的你听懂了吗？（3--6岁）',
+    'enworkid': '/s/tbDgGfP4KLhVS_H9c0xjoA',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/202.mp3',
+    'type': 'MP3'
+  }, {
+    'songname': '交流密码：我说的你听懂了吗？（7--12岁）',
+    'enworkid': '/s/nj0hndV7qXICtcA-OSbkUQ',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/203.mp3',
+    'type': 'MP3'
+  }, {
+    'songname': '令人满意和令人不快的称赞',
+    'enworkid': '/s/xRkKN0oFne4wAXuQj5Abyw',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/204.mp3',
+    'type': 'MP3'
+  }, {
+    'songname': '无效沟通，有效沟通',
+    'enworkid': '/s/xRkKN0oFne4wAXuQj5Abyw',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/205.mp3',
+    'type': 'MP3'
+  }, {
+    'songname': '学会称赞',
+    'enworkid': '/s/xRkKN0oFne4wAXuQj5Abyw',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/206.mp3',
+    'type': 'MP3'
+  }, {
+    'songname': '找到更好的解决方法',
+    'enworkid': '/s/xRkKN0oFne4wAXuQj5Abyw',
+    'src': 'https://media.alearning.com.cn/podcast/book/2/207.mp3',
+    'type': 'MP3'
+  }]
+}, {
+  id: '1',
+  status: '',
+  name: '好妈妈胜过好老师',
+  title: '好妈妈胜过好老师',
+  cover: 'https://media.alearning.com.cn/podcast/book/1/cover.jpg',
+  detail: '',
+  author: {
+    name: '尹建莉',
+    avatar: 'https://media.alearning.com.cn/podcast/book/1/author.jpg',
+    detail: '尹建莉，教育专家，从教多年，她的《好妈妈胜过好老师》紧贴当下生活，讲了许多教育故事，它让人看到，许多人们习以为常的教育行为，原来潜伏着错误；许多被人们忽略的小事，正是教育中最重要的良机。它让家长反思、醒悟，使家长对教育子女茅塞顿开。尹建莉的女儿品学兼优，曾跳级两次，16岁参加高考，取得了超过当年清华录取线22分的优异成绩。每个孩子的优秀都是有源头的。从书中可以看到，她对女儿何等用心。'
+  },
+  anchor: {
+    name: '毕艳锋',
+    detail: '麻辣贤师 拉风知识分子',
+    avatar: 'https://media.alearning.com.cn/podcast/book/1/anchor.jpg'
+  },
+  type: 'book',
+  list: [{
+    id: '',
+    songname: '朗读者和作者介绍、内容简介',
+    src: 'https://media.alearning.com.cn/podcast/book/1/101.mp3',
+    type: 'mp3'
+  }, {
+    id: '',
+    songname: '如何让孩子理性面对困难',
+    src: 'https://media.alearning.com.cn/podcast/book/1/102.mp3',
+    type: 'mp3'
+  }, {
+    id: '',
+    songname: '如何让孩子自觉地少看电视',
+    src: 'https://media.alearning.com.cn/podcast/book/1/103.mp3',
+    type: 'mp3'
+  }, {
+    id: '',
+    songname: '被魔杖点中的孩子学习能力强',
+    src: 'https://media.alearning.com.cn/podcast/book/1/104.mp3',
+    type: 'mp3'
+  }, {
+    id: '',
+    songname: '阅读需要诱惑',
+    src: 'https://media.alearning.com.cn/podcast/book/1/105.mp3',
+    type: 'mp3'
+  }, {
+    id: '',
+    songname: '好阅读，坏阅读',
+    src: 'https://media.alearning.com.cn/podcast/book/1/106.mp3',
+    type: 'mp3'
+  }, {
+    id: '',
+    songname: '不看”有用“的书',
+    src: 'https://media.alearning.com.cn/podcast/book/1/107.mp3',
+    type: 'mp3'
+  }]
+}, {
+  id: '3',
+  status: '',
+  name: '我祝你经历不公、遭遇背叛，感受孤独',
+  title: '我祝你经历不公、遭遇背叛，感受孤独',
+  cover: 'https://media.alearning.com.cn/podcast/article/1/cover.jpg',
+  detail: '',
+  author: {
+    avatar: 'https://media.alearning.com.cn/podcast/article/1/author.jpg',
+    detail: '2005年9月，由布什总统提名，参议院批准通过，罗伯茨就任美国联邦最高法院第17任首席大法官。他也是美国两个世纪以来最年轻的首席大法官。'
+  },
+  anchor: {
+    name: '叶青',
+    detail: '高冷鲜师 理性思辨者 幕后英雄',
+    avatar: 'https://media.alearning.com.cn/podcast/book/2/anchor.jpg'
+  },
+  type: 'article',
+  list: [{
+    id: '',
+    songname: '我祝你经历不公、遭遇背叛，感受孤独',
+    src: 'https://media.alearning.com.cn/podcast/article/1/101.mp3',
+    type: 'mp3'
+  }]
+}, {
+  id: '4',
+  status: '',
+  name: '忍不住吼孩子',
+  title: '忍不住吼孩子',
+  cover: 'https://media.alearning.com.cn/podcast/article/2/cover3.jpg',
+  detail: '父母的吼叫有时候会成为孩子一生的阴影，很多研究表明，长期对孩子发脾气、吼叫，不仅会造成亲子关系的疏离，也会让孩子在成长过程中形成自我否定的消极心理，严重的还会影响他们与别人建立亲密关系的能力。然而，这些道理很多人懂，做到却好难好难。',
+  author: {
+    avatar: '',
+    detail: ''
+  },
+  anchor: {
+    name: '李潇',
+    detail: '和蔼可亲 热情幽默 靠谱女青年',
+    avatar: 'https://media.alearning.com.cn/podcast/article/2/anchor.jpg'
+  },
+  type: 'article',
+  list: [{
+    id: '',
+    songname: '忍不住吼孩子',
+    src: 'https://media.alearning.com.cn/podcast/article/2/201.mp3'
+  }]
+}, {
+  id: '5',
+  status: '',
+  name: '有话好好说-1',
+  title: '有话好好说-1',
+  cover: 'https://media.alearning.com.cn/podcast/article/1/cover2.JPG',
+  detail: '语言是带情绪的，你所说的每一个字串联起来，有可能给人带去温暖，但也可能带来伤害。“打击式教育”出来的孩子不能坚持自我，很容易生气，不懂得尊重别人的感受，行为粗鲁没有礼貌……。\n',
+  author: {
+    avatar: '',
+    detail: ''
+  },
+  anchor: {
+    name: '李潇',
+    detail: '和蔼可亲 热情幽默 靠谱女青年',
+    avatar: 'https://media.alearning.com.cn/podcast/article/2/anchor.jpg'
+  },
+  type: 'article',
+  list: [{
+    id: '',
+    songname: '有话好好说',
+    src: 'https://media.alearning.com.cn/podcast/article/3/1.mp3'
+  }]
+}, {
+  id: '6',
+  status: '',
+  name: '这个世界没有笨小孩',
+  title: '这个世界没有笨小孩',
+  cover: 'https://media.alearning.com.cn/podcast/article/4/cover.jpg',
+  detail: '绝大部分人智商差异不大，只是特长不同而已。有的孩子虽然读书不行，但也许在其他方面有很大的潜力；有的孩子刚开始上小学时成绩不是特别好，但是后劲足，潜力大。',
+  author: {
+    avatar: '',
+    detail: ''
+  },
+  anchor: {
+    name: '李潇',
+    detail: '和蔼可亲 热情幽默 靠谱女青年',
+    avatar: 'https://media.alearning.com.cn/podcast/article/2/anchor.jpg'
+  },
+  type: 'article',
+  list: [{
+    id: '',
+    songname: '这个世界没有笨小孩',
+    src: 'https://media.alearning.com.cn/podcast/article/4/1.mp3'
+  }]
+}]
