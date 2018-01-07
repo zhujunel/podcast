630205a0219b1b4eef0fa028fd9b582784f4bfca
baisheng <baisheng@outlook.com>
2018-01-07 23:19:44 +0800

    update

--- src/utils/WxUtils.js
+++ src/utils/WxUtils.js
@@ -1,42 +1,71 @@
 /* eslint-disable no-undef */
 import Tips from './Tips'
+import wepy from 'wepy'
 
 export default class WxUtils {
   /**
    * 如果能够后退（多层），则navigaetBack，否则调用redirectTo
    */
   static backOrRedirect(url) {
-    const pages = getCurrentPages()
+    const pages = getCurrentPages();
     // route在低版本不兼容
-    const index = pages.findIndex(item => ('/' + item.__route__) === url)
+    const index = pages.findIndex(item => ('/' + item.__route__) == url)
     if (pages.length < 2 || index < 0) {
       wx.redirectTo({
         url: url
-      })
+      });
     } else {
-      const delta = pages.length - 1 - index
+      const delta = pages.length - 1 - index;
       wx.navigateBack({
         delta: delta
-      })
+      });
     }
   }
   /**
    * 如果能够后退（多层），则navigaetBack，否则调用navigateTo
    */
   static backOrNavigate(url) {
-    const pages = getCurrentPages()
+    const pages = getCurrentPages();
     // route在低版本不兼容
-    const index = pages.findIndex(item => ('/' + item.__route__) === url)
+    const index = pages.findIndex(item => ('/' + item.__route__) == url)
     if (pages.length < 2 || index < 0) {
       wx.navigateTo({
         url: url
-      })
+      });
     } else {
-      const delta = pages.length - 1 - index
+      const delta = pages.length - 1 - index;
       wx.navigateBack({
         delta: delta
-      })
+      });
     }
+  }
+
+  /**
+   * 选择图标（最大大小限制）
+   */
+  static chooseImage(param, maxSize) {
+    Tips.loading();
+    return wepy.chooseImage(param).then(async ({tempFilePaths, tempFiles}) => {
+      if (tempFiles && maxSize) {
+        const removeIndex = []
+        tempFiles.forEach((file, index) => {
+          const limit = maxSize * 1024 * 1024
+          if (file.size > limit) {
+            removeIndex.push(index)
+          }
+        });
+        const posStr = removeIndex.map(v => v + 1).join(',')
+        if (removeIndex.length > 0) {
+          removeIndex.forEach(i => tempFilePaths.splice(i, 1))
+          await Tips.alert(`第${posStr}张图超过${maxSize}M`)
+        }
+      }
+      Tips.loaded()
+      return tempFilePaths
+    }).catch(() => {
+      Tips.loaded()
+      return []
+    })
   }
 
   static wxPay(param) {
